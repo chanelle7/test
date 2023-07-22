4c2a4437f6f7214a582ae1e020926bcf52474c8c
Chanelle <cqw668@gmail.com>
2023-07-22 04:15:52 -0400

    repeat 4 times with a for loo

---
+++ draw.py
@@ -0,0 +1,17 @@
+#import turtle
+#painter = turtle.Turtle()
+#painter.forward(100)
+#painter.left(90)
+#turtle.done()
+
+
+import turtle
+painter = turtle.Turtle()
+
+for _ in range(4):
+    painter.forward(100)
+    painter.left(90)
+
+#painter.forward(100)
+#painter.left(90)
+turtle.done()
